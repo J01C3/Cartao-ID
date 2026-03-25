73931f8353d409a064ee704376047c678b7575fc
D3vJoy <168494262+J01C3@users.noreply.github.com>
2026-03-25 18:37:13 -0300

    meu app ionic

--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -3,10 +3,10 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
   standalone: true,
   imports: [IonicModule, RouterModule]
 })
-export class HomePage {}
+export class SobrePage {}
